c43c1f5c62d6f9afacedca12185cf2293f159cc3
Kihwan Cho <ozlongblack@gmail.com>
2020-11-18 23:23:25 +1100

    add header component

---
+++ src/pages/App/App.page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from '../../components/Header/Header.component';
+
+const Home = () => (
+  <div>
+    <Header></Header>
+    App page
+  </div>
+);
+
+export default Home;
